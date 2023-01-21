048902775679cff65a62170b266c033f4c592efc
victor-7-7 <Victor.7.7@ya.ru>
2023-01-21 12:53:27 +0700

    Build the navbar component.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 
+// JS-движок составляет список всех элементов класса collapsible на веб-странице
 const collapsibles = document.querySelectorAll(".collapsible");
 
 collapsibles.forEach((item) =>
+  // На каждый элемент класса collapsible (item) движок вешает клик-коллбэк
   item.addEventListener("click", function () {
+    // У текущего элемента движок смотрит список класс-атрибутов. 
+    // Если в списке нет атрибута collapsible--expanded, то движок 
+    // добавляет его, а если есть, то - удаляет.  
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+
+
+
